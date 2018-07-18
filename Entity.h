ea68194d164f510711f76fc6bd47615c8e5d88c6
smclose <smclose@student.cs.uwaterloo.ca>
2018-07-18 19:47:59 -0400

    Added base Entity class

---
+++ Entity.h
@@ -0,0 +1,16 @@
+#ifndef ENTITY_H
+#define ENTITY_H
+
+#include "Observer.h"
+
+class Entity: public Observer {
+	size_t x, y;
+	char icon;
+public:
+	Entity(size_t x, size_t y, char icon) : x{x}, y{y}, icon{icon};
+
+	char print() { return icon; }
+	virtual ~Entity() = 0;
+}
+
+#endif
