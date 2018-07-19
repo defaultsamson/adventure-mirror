67f2e84f3b0c3af0e0b2b70301eb9d395cc2a78a
Yi Jie Wang <yj26wang@edu.uwaterloo.ca>
2018-07-18 20:34:07 -0400

    Implemented the header files for the map objects

---
+++ Pathway.h
@@ -0,0 +1,11 @@
+#ifndef PATHWAY_H
+#define PATHWAY_H
+
+#include "Entity.h"
+
+class Pathway : public Entity{
+        public :
+        Pathway();
+        void notify();
+}
+#endif
---
+++ Wall.h
@@ -0,0 +1,11 @@
+#ifndef WALL_H
+#define WALL_H
+
+#include "Entity.h"
+
+class Wall : public Entity{
+        public :
+        Wall();
+        void notify();
+}
+#endif
