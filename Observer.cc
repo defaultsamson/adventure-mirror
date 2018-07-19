5aa5aebd2b148b8b5cbd6bfc34bef8bc15427f68
Yi Jie Wang <yj26wang@edu.uwaterloo.ca>
2018-07-18 20:08:39 -0400

    Implemented observer.cc

---
+++ Observer.cc
@@ -0,0 +1,12 @@
+#include "Observer.h"
+//Call the notify function on every observers in the observer vector
+void Observer::notifyObservers(){
+	for (Observer *ob = observers.begin(); ob != observers.end(); ++ob){
+		notifty(*ob);
+	}
+}
+
+//Add a new observer to this subject
+void Observer::attach(Observer *ob){
+	observers.push_back(ob);
+}
