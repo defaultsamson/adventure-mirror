4e66b5c624c9ea56c2dd147e0e2dbf5db8b571a9
smclose <smclose@student.cs.uwaterloo.ca>
2018-07-21 19:54:42 -0400

    Fixed printing segmentation fault

--- StaticEntity.cc
+++ StaticEntity.cc
@@ -6,5 +6,5 @@ size_t StaticEntity::getX() { return x; }
 size_t StaticEntity::getY() { return y; }
 void StaticEntity::setX(size_t x) { this->x = x; }
 void StaticEntity::setY(size_t y) { this->y = y; }
-char StaticEntity::print() { return this->print(); }
+char StaticEntity::print() { return icon; }
 
