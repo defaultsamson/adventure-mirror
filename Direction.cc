82e623023e18841fc14b45dbf57aa6d6bf1a56a5
William <belguawhale@gmail.com>
2018-07-21 16:33:36 -0400

    Added Direction class

---
+++ Direction.cc
@@ -0,0 +1,43 @@
+#include "Direction.h"
+
+Direction::Direction(int x=0, int y=0): x{x}, y{y} {}
+
+const Direction Direction::N {0,-1};
+const Direction Direction::NE {1, -1};
+const Direction Direction::E{1, 0};
+const Direction Direction::SE{1, 1};
+const Direction Direction::S{0, 1};
+const Direction Direction::SW{-1, 1};
+const Direction Direction::W{-1, 0};
+const Direction Direction::NW{-1, -1};
+const Direction Direction::Invalid{0, 0};
+
+Direction Direction::getDirection(std::string d) {
+    if (d == "no") {
+        return Direction::N;
+    }
+    else if (d == "so") {
+        return Direction::S;
+    }
+    else if (d == "ea") {
+        return Direction::E;
+    }
+    else if (d == "we") {
+        return Direction::W;
+    }
+    else if (d == "ne") {
+        return Direction::NE;
+    }
+    else if (d == "nw") {
+        return Direction::NW;
+    }
+    else if (d == "se") {
+        return Direction::SE;
+    }
+    else if (d == "sw") {
+        return Direction::SW;
+    }
+    else {
+        return Direction::Invalid;
+    }
+}
