432924b90cfc64786622f05579537d1b17b2be49
smclose <smclose@student.cs.uwaterloo.ca>
2018-07-21 20:27:21 -0400

    Fixed bug

--- Stair.cc
+++ Stair.cc
@@ -1,7 +1,7 @@
 #include "Observer.h"
 #include "Stair.h"
 
-Stair::Stair(size_t x, size_t y) : StaticEntity{x, y, '\\'}
+Stair::Stair(size_t x, size_t y) : StaticEntity{x, y, '\\'} {}
 
 void Stair::notify(Observer &o) {
 	(void) o;
