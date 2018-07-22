cd8fa598e6ba1e7e700df90b3f76d60450e9f996
William <belguawhale@gmail.com>
2018-07-21 22:29:19 -0400

    fixed a merge error

--- BaseCharacter.cc
+++ BaseCharacter.cc
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-#include "BaseCharacter.h"
-#include "Character.h"
-#include "Observer.h"
-
-BaseCharacter::BaseCharacter(size_t x, size_t y, char icon, int hp, int maxHp, int atk, int def) : x{x}, y{y}, icon{icon}, hp{hp}, maxHp{maxHp}, atk{atk}, def{def} {}
-
-void BaseCharacter::hit(Character &other) {
-	// Class specific attacks can override this. By default, deal the attack
-	other.damage(*this, atk);
-}
-
-void BaseCharacter::damage(Character &other, int damage) {
-	(void) other;
-	// Class specific defences can override this. By default, take the damage
-	hp -= damage;
-}
-
-// This should be checked every time this character takes damage
-void BaseCharacter::deathCheck() {
-	if (hp <= 0) {
-		// TODO Default death behaviour (e.g. remove from the floor)
-	}
-}
-
-int BaseCharacter::score() { return gold; }
-int BaseCharacter::getHP() { return hp; }
-int BaseCharacter::getMaxHP() { return maxHp; }
-int BaseCharacter::getAtk() { return atk; }
-int BaseCharacter::getDef() { return def; }
-void BaseCharacter::addGold(int value) { gold += value; }
-int BaseCharacter::getGold() { return gold; }
-int BaseCharacter::getFloor() { return floor; }
-size_t BaseCharacter::getX() { return x; }
-size_t BaseCharacter::getY() { return y; }
-void BaseCharacter::setX(size_t x) { this->x = x; }
-void BaseCharacter::setY(size_t y) { this->y = y; }
-char BaseCharacter::print() { return icon; }
-
-CharacterType BaseCharacter::getType() { return type;}
-
-void BaseCharacter::notify(Observer &o) {
-	(void) o;
-}
-
-=======
 #include "BaseCharacter.h"
 #include "Character.h"
 #include "Observer.h"
@@ -88,4 +42,3 @@ void BaseCharacter::notify(Observer &o) {
 	(void) o;
 }
 
->>>>>>> 7a236811c57b710c82e0c7e2b0717e95c376d351
--- BaseCharacter.h
+++ BaseCharacter.h
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-#ifndef BASECHARACTER_H
-#define BASECHARACTER_H
-
-#include "Observer.h"
-#include "Character.h"
-#include "Item.h"
-
-class BaseCharacter: public Character {
-	size_t x, y;
-	char icon;
-	int hp;
-	int maxHp;
-	int atk;
-	int def;
-	int gold = 0;
-	int floor = 0;
-	CharacterType type;
-protected:
-	void deathCheck() override;
-public:
-	BaseCharacter(size_t x, size_t y, char icon, int hp, int maxHp, int atk, int def);
-	void hit(Character &other) override;
-	void damage(Character &other, int damage) override;
-	int score() override;
-	int getHP() override;
-	int getMaxHP() override;
-	int getAtk() override;
-	int getDef() override;
-	void addGold(int value) override;
-	int getGold() override;
-	int getFloor() override;
-	size_t getX() override;
-	size_t getY() override;
-	void setX(size_t x) override;
-	void setY(size_t y) override;
-	char print() override;
-	void notify(Observer &o) override;
-	CharacterType getType();
-};
-
-#endif
-=======
 #ifndef BASECHARACTER_H
 #define BASECHARACTER_H
 
@@ -82,4 +39,3 @@ public:
 };
 
 #endif
->>>>>>> 7a236811c57b710c82e0c7e2b0717e95c376d351
