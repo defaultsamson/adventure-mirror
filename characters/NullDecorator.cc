ba95fdc08e6e929a470bd954e4b8f1e41a363197
smclose <smclose@student.cs.uwaterloo.ca>
2018-07-25 23:53:04 -0400

    dong

--- characters/NullDecorator.cc
+++ characters/NullDecorator.cc
@@ -2,6 +2,7 @@
 #include <string>
 #include "CharacterDecorator.h"
 #include <math.h>
+#include "CharacterType.h"
 
 using namespace std;
 
@@ -16,6 +17,13 @@ bool NullDecorator::hit(Character &other, string &output) {
         else {
                 output += " missed! ";
         }
+
+	// Note: Keep the other pointer alive in DungeonMap even if the character is dead so that this will run properly
+        if (component->getType() == CharacterType::Goblin && other.deathCheck()) {
+                addGold(5);
+                output += "Goblin steals an extra 5 treasure from the " + other.getType().to_string() + "'s body! ";
+        }
+
 	return result;
 }
 
